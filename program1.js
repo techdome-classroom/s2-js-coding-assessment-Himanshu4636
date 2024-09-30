cafc4d3150438348b33a384c2e7b7f2d9bb21fdf
Himanshu Gurjar <gurjarhimanshu682@gmail.com>
2024-09-30 15:17:24 +0530

    Completed programs and passed tests

--- program1.js
+++ program1.js
@@ -2,10 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+    const stack = [];
+    const map = {
+        ")": "(",
+        "}": "{",
+        "]": "[",
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        } else if (char === ")" || char === "}" || char === "]") {
+            if (stack.length === 0 || stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -2,9 +2,36 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+var romanToInt = function (s) {
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    let total = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+
+        // If the current value is less than the previous value, we subtract it
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            // Otherwise, we add it
+            total += currentValue;
+        }
+
+        // Update previous value
+        prevValue = currentValue;
+    }
+
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
